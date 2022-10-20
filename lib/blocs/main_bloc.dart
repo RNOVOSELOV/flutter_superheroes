c99becd572ffa3c84f875e847931c93c1852f157
RNOVOSELOV <novoselov_roman@mail.ru>
2022-10-20 11:59:38 +0300

    4_1

--- lib/blocs/main_bloc.dart
+++ lib/blocs/main_bloc.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:http/http.dart' as http;
-import 'package:superheroes/models/superhero.dart';
+import 'package:superheroes/model/superhero.dart';
 
 class MainBloc {
   static const minSymbols = 3;
